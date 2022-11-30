91152cbed67031e236f72f7cc1b54b0cd69ff7cb
beboopr <linnets-router.09@icloud.com>
2022-11-30 18:22:33 -0500

    added img tag to display image on front end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Navbar from './components/Navbar';
 import PostList from './components/PostList';
 import Footer from './components/Footer'
 import './App.css';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <PostList />
+      <Upload />
       <Footer />
+      
     </div>
   );
 }
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,50 +1,55 @@
 //uploading pictures here
 import React, { useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getStorage, ref, StorageReference, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXCI7-Qg7g0yEOMdlWRdrlV_fkweAH204",
-    authDomain: "united-backk.firebaseapp.com",
-    projectId: "united-backk",
-    storageBucket: "united-backk.appspot.com",
-    messagingSenderId: "196638236647",
-    appId: "1:196638236647:web:e1fe7366d29e268ba02742"
-  };
+  apiKey: "AIzaSyDXCI7-Qg7g0yEOMdlWRdrlV_fkweAH204",
+  authDomain: "united-backk.firebaseapp.com",
+  projectId: "united-backk",
+  storageBucket: "united-backk.appspot.com",
+  messagingSenderId: "196638236647",
+  appId: "1:196638236647:web:e1fe7366d29e268ba02742",
+};
 
-  export default function Upload() {
-    const [selectedFile, setSelectedFile] = useState<File | undefined>();
-    console.log({selectedFile});
-    const handleUpload = (e: React.FormEvent<HTMLElement>) => {
-      e.preventDefault();
-      if(!selectedFile) {
-        alert("Please select a file first!")
-          
+export default function Upload() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [url, setUrl] = useState();
+  console.log({ selectedFile });
+  const handleUpload = (e) => {
+    e.preventDefault();
+    if (!selectedFile) {
+      alert("Please select a file to Upload!");
     }
     // connect to firebase project
     const app = initializeApp(firebaseConfig);
-   
+
     // connect to our storage bucket
     const storage = getStorage(app);
-   
+
     // create a reference to our file in storage
     const filename = selectedFile?.name;
-    const imageRef: StorageReference = ref(storage, 'photos/' + filename);
-    
-    // (Todd's quick cheat) create the url from reference
-    const url = `https://firebasestorage.googleapis.com/v0/b/upload-storage-c8.appspot.com/o/photos%2F${filename}?alt=media`
-   
+    const imageRef = ref(storage, "photos/" + filename);
+
     // upload file to bucket
-    uploadBytes(imageRef, selectedFile);
+    uploadBytes(imageRef, selectedFile).then(() => {
+    // create the url from reference
+      setUrl(
+        `https://firebasestorage.googleapis.com/v0/b/united-backk.appspot.com/o/photos%2F${filename}?alt=media`
+      );
     // add an await or .then and then update our database with url
-  }
+      alert("successfuly uploaded img to " + url);
+    });
+  };
   return (
     <form onSubmit={handleUpload}>
-      <input type="file" name="photo"
-        onChange={(e: React.FormEvent<HTMLInputElement> | any) => setSelectedFile(e.currentTarget.files[0])}
-        // value={selectedFile.name}
-        />
+      {url && <img src={url} alt="" width={300} />}
+      <input
+        type="file"
+        name="photo"
+        onChange={(e) => setSelectedFile(e.currentTarget.files[0])}
+      />
       <button type="submit">Upload</button>
     </form>
-  )
+  );
 }
